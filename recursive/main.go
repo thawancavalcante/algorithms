e43a790207b26b59d6142aba34e24ba4f255b912
Thawan Cavalcante <thawan.cavalcante@idog.app>
2023-09-28 12:51:25 -0300

    recursive

--- recursive/main.go
+++ recursive/main.go
@@ -5,22 +5,16 @@ import (
 )
 
 func main() {
-	//Result: 25x25
 	fmt.Println(findLargestSquareSizeHandle(25, 40))
 
-	//Result: 20x20
 	fmt.Println(findLargestSquareSizeHandle(20, 50))
 
-	//Result: 3x3
 	fmt.Println(findLargestSquareSizeHandle(33, 30))
 
-	//Result: 200x200
 	fmt.Println(findLargestSquareSizeHandle(200, 250))
 
-	//Result: 200x200
 	fmt.Println(findLargestSquareSizeHandle(200, 500))
 
-	//Result: 250x250
 	fmt.Println(findLargestSquareSizeHandle(300, 250))
 }
 
