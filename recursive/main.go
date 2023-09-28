74a3e890dc37f9d6c43e362f1ffc034fa11b6aed
Thawan Cavalcante <thawan.cavalcante@idog.app>
2023-09-28 13:15:18 -0300

    recursive

--- recursive/main.go
+++ recursive/main.go
@@ -6,16 +6,14 @@ import (
 
 func main() {
 	fmt.Println(findLargestSquareSizeHandle(25, 40))
-
 	fmt.Println(findLargestSquareSizeHandle(20, 50))
-
 	fmt.Println(findLargestSquareSizeHandle(33, 30))
-
 	fmt.Println(findLargestSquareSizeHandle(200, 250))
-
 	fmt.Println(findLargestSquareSizeHandle(200, 500))
-
 	fmt.Println(findLargestSquareSizeHandle(300, 250))
+
+	values := []int{1, 4, 10, 100, 2, 3, -1, -50, 5, 99, 10, 2}
+	fmt.Println(sum(values, 0))
 }
 
 func findLargestSquareSizeHandle(horizontalSize int, verticalSize int) (int, int) {
@@ -39,4 +37,12 @@ func findLargestSquareSize(bigSize int, smallSize int) (int, int) {
 	}
 
 	return findLargestSquareSize(smallSize, newSize)
+}
+
+func sum(values []int, total int) int {
+	if len(values) == 0 {
+		return total
+	}
+
+	return sum(values[1:], values[0]+total)
 }
